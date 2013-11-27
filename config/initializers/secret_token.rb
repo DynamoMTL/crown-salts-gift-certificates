465a052a476dd88511edb8f138f2dc3884bde1ee
Jonathan Hester <hester.jonathan@gmail.com>
2013-11-27 12:55:26 -0800

    new rails project with stripe gem and annotate gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StripeTest::Application.config.secret_key_base = '960c585c5cc0262c9843ded1ed130d0a39c41b7b2aa868f69069712ccf8840d55ee94091fe2c678fb6f4840e9f24c34acbe22c7426c246d674f70e3694b82c73'
